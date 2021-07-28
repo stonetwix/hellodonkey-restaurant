8baf5a1f129bb44bd53981c4aa83726e657cbade
Moa Stenqvist <moa@stonetwig.se>
2021-07-28 16:27:45 +0200

    Fix headline leftovers.

--- client/src/components/Leftovers/LeftoverCardGrid.tsx
+++ client/src/components/Leftovers/LeftoverCardGrid.tsx
@@ -1,81 +1,89 @@
 import React, { CSSProperties } from 'react';
 import { Card, Col, Row } from 'antd';
+import background from '../../assets/yellow-dotted-bg.png';
 
 
 class LeftOverCardGrid extends React.Component {
 
     render() {
         return (
-            <div className="site-card-wrapper" style={containerStyle}>
-                <Row gutter={20} style={{ marginBottom: '2rem' }}>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
-                    </Col>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
-                    </Col>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
+            <>
+                <Row style={bgStyle}>
+                    <Col style={headlineStyle}>
+                        <h1>Buy our leftovers</h1>
                     </Col>
                 </Row>
-                <Row gutter={20} style={{ marginBottom: '2rem' }}>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
-                    </Col>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
-                    </Col>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
-                    </Col>
-                </Row>
-                <Row gutter={20} style={{  marginBottom: '2rem'}}>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
-                    </Col>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
-                    </Col>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
-                    </Col>
-                </Row>
-                <Row gutter={20} style={{ marginBottom: '2rem'}}>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
-                    </Col>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
-                    </Col>
-                    <Col span={8}>
-                        <Card title="Card title" bordered={true}>
-                            Card content
-                        </Card>
-                    </Col>
-                </Row>
-            </div>
+                <div className="site-card-wrapper" style={containerStyle}>
+                    <Row gutter={20} style={{ marginBottom: '2rem' }}>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row gutter={20} style={{ marginBottom: '2rem' }}>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row gutter={20} style={{  marginBottom: '2rem'}}>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                    </Row>
+                    <Row gutter={20} style={{ marginBottom: '2rem'}}>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                        <Col span={8}>
+                            <Card title="Card title" bordered={true}>
+                                Card content
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
+            </>
         )
     }
 }
@@ -87,5 +95,23 @@ const containerStyle: CSSProperties = {
     flexDirection: 'column',
     justifyContent: 'center',
     width: '80%',
-    margin: '10rem auto'
+    margin: '8rem auto'
+}
+
+const bgStyle: CSSProperties = {
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    backgroundPosition: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+}
+
+const headlineStyle: CSSProperties = {
+    marginTop: '10rem',
+    marginBottom: '2rem',
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '2rem 0'
 }
