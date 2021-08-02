92c400f7cfdae137df370df3576c07feadda09d9
Moa Stenqvist <moa@stonetwig.se>
2021-08-02 09:48:40 +0200

    Add dynamic rendering of leftover cards.

--- client/src/components/Leftovers/LeftoverCardGrid.tsx
+++ client/src/components/Leftovers/LeftoverCardGrid.tsx
@@ -1,11 +1,34 @@
 import React, { CSSProperties } from 'react';
-import { Card, Col, Row } from 'antd';
+import { Card, Col, List, Row } from 'antd';
 import background from '../../assets/yellow-dotted-bg.png';
+import { Food } from '../Takeaway/TakeawayView';
+import { PlusCircleFilled } from '@ant-design/icons';
+import { Spinner } from 'react-bootstrap';
 
+interface State {
+    foods: Food[];
+    loading: boolean;
+}
 
-class LeftOverCardGrid extends React.Component {
+class LeftOverCardGrid extends React.Component<{}, State> {
+    state: State = {
+        foods: [],
+        loading: true,
+    }
+
+    async componentDidMount() {
+        const foods = await getFoods();
+        this.setState({ foods: foods, loading: false });
+    }
 
     render() {
+        if (this.state.loading) {
+            return (
+                <div style={{textAlign: 'center', width: '100%', height: '100%'}}>
+                    <Spinner animation="grow" />
+                </div>
+            )
+        }
         return (
             <>
                 <Row style={bgStyle}>
@@ -15,72 +38,31 @@ class LeftOverCardGrid extends React.Component {
                 </Row>
                 <div className="site-card-wrapper" style={containerStyle}>
                     <Row gutter={20} style={{ marginBottom: '2rem' }}>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Row gutter={20} style={{ marginBottom: '2rem' }}>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Row gutter={20} style={{  marginBottom: '2rem'}}>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Row gutter={20} style={{ marginBottom: '2rem'}}>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
-                        <Col span={8}>
-                            <Card title="Card title" bordered={true}>
-                                Card content
-                            </Card>
-                        </Col>
+                    <List
+                        grid={{
+                        gutter: 16,
+                        xs: 1,
+                        sm: 2,
+                        md: 3,
+                        lg: 3,
+                        xl: 3,
+                        xxl: 3,
+                        }}
+                        dataSource={this.state.foods}
+                        renderItem={item => (
+                        <List.Item>
+                            <Card 
+                                actions={[
+                                    <PlusCircleFilled style={addIconStyle}/>,
+                                ]}
+                                title={item.title} 
+                                extra={Math.floor(item.price / 2) + ' kr'}
+                            >
+                                {item.description}
+                            </Card>                 
+                        </List.Item>
+                        )}
+                    />
                     </Row>
                 </div>
             </>
@@ -114,4 +96,23 @@ const headlineStyle: CSSProperties = {
     display: 'flex',
     justifyContent: 'center',
     padding: '2rem 0'
+}
+
+const addIconStyle: CSSProperties = {
+    marginTop: '0.4rem',
+    color: '#61c9a8',
+    cursor: 'pointer',
+    fontSize: '1.5rem'
+}
+
+const getFoods = async () => {
+    try {
+        let response = await fetch('/api/foods');
+        if (response.ok) {
+          const data = await response.json();
+          return data;
+        }
+    } catch (error) {
+        console.error(error);
+    }
 }
