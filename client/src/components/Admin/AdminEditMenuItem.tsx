14483962ee42bb5bc93dcbc1979fd3015c3f7f38
Moa Stenqvist <moa@stonetwig.se>
2021-08-09 10:03:47 +0200

    Add 'tag selection' to edit form.

--- client/src/components/Admin/AdminEditMenuItem.tsx
+++ client/src/components/Admin/AdminEditMenuItem.tsx
@@ -1,16 +1,19 @@
-import { Button, Form, Input, InputNumber, Layout, message } from "antd";
+import { Button, Form, Input, InputNumber, Layout, message, Select } from "antd";
 import { CSSProperties, useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
-import { useLocation, withRouter } from "react-router-dom";
+import { useHistory, useLocation, withRouter } from "react-router-dom";
 import { Food } from "../Takeaway/TakeawayView";
 import SiderMenu from './Sider';
 
 const { Content } = Layout;
+const { Option } = Select;
 
 const layout = {
     labelCol: { span: 4 },
     wrapperCol: { span: 16 },
-  };
+};
+
+const children: string[] = ['Vegan', 'Vegetarian', 'Lactose free', 'Gluten free'];
   
   /* eslint-disable no-template-curly-in-string */
   const validateMessages = {
@@ -27,6 +30,7 @@ const layout = {
 
 const AdminEditMenuItem = () => {
     let location = useLocation();
+    let history = useHistory();
     const [menuItem, setMenuItem] = useState<Food>();
     const [loading, setLoading] = useState(true);
 
@@ -36,21 +40,34 @@ const AdminEditMenuItem = () => {
             setLoading(false);
         }
         fetchData()
-    }, [location.pathname]);
+    }, [location]);
 
     const onFinish = (values: any) => {
         console.log(values);
         successSave();
+        history.push('/admin/menu');
     };
 
     const successSave = () => {
         message.success({
-            content: 'Menu item updated',
+            content: 'Menu item has been updated',
             style: {
               marginTop: '6rem',
             },
         });
     };
+
+    const handleTagChange = (value: any) => {
+        console.log(`selected ${value}`);
+    }
+
+    const tagOptions = () => {
+        return children.map((c: string) => {
+            return <Option value={c} key={c}>
+                {c}
+            </Option>
+        })
+    }
 
     if (loading) {
         return (
@@ -82,17 +99,29 @@ const AdminEditMenuItem = () => {
                         <Input />
                     </Form.Item>
                     <Form.Item name={['menuItem', 'description']} label="Description" rules={[{ required: true }]}>
-                        <Input.TextArea />
+                        <Input.TextArea rows={4}/>
                     </Form.Item>
                     <Form.Item name={['menuItem', 'price']} label="Price" rules={[{ required: true, type: 'number' }]}>
                         <InputNumber />
+                    </Form.Item>
+                    <Form.Item name={['menuItem', 'tags']} label="Tags" >
+                    <Select
+                        mode="multiple"
+                        allowClear
+                        style={{ width: '100%' }}
+                        placeholder="Please select"
+                        //defaultValue={['a10', 'c12']}
+                        onChange={handleTagChange}
+                    >
+                        {tagOptions()}
+                    </Select>
                     </Form.Item>
                     <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 4 }}>
                         <Button 
                             type="primary" 
                             htmlType="submit"
                         >
-                            Submit
+                            Save
                         </Button>
                     </Form.Item>
                     </Form>
